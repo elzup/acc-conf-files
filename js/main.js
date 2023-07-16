810697939227fbf06855a7a035b5b5bd483e9011
elzup <guild0105@gmail.com>
2023-07-16 23:25:31 +0900

    add templates

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// @flow
+
+const main = (input /*: string */) => {
+  const r = reader(input);
+  const n = r.n();
+
+  console.log(["SUN", "SAT", "FRI", "THU", "WED", "TUE", "MON"].indexOf(n);)
+};
+
+const reader = (str /*: string*/) => {
+  const lines = str.trim().split("\n");
+  const s = () => lines.shift() || "";
+  const n = () => Number(s());
+
+  const ss = () => s().split(" ");
+  const ns = () => ss().map(v => Number(v));
+  return { lines, s, n, ss, ns };
+};
+
+const getInput = () => require("fs").readFileSync("/dev/stdin", "utf8");
+main(getInput());
---
+++ rs/main.rs
@@ -0,0 +1,14 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
+fn smain() {
+    input! {
+        N: u32,
+    }
+    return N;
+}
+
+fn main() {
+    println!("{}", smain());
+}
